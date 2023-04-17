0f5eaeb004c719ce2d911b746668904e49786575
Popsicool <akinolasamson1234@gmail.com>
2023-04-17 10:38:31 +0100

    advance tasks solution

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject (report) {
+  const list = [];
+  const keys = Object.keys(report.allEmployees);
+  keys.forEach((key, index) => {
+    for (const name of report.allEmployees[key]) {
+      list.push(name);
+    }
+  });
+  return list;
+}
