e2470ffa5af00e6b1092b35fc405a2425814c19c
Popsicool <akinolasamson1234@gmail.com>
2023-04-17 10:16:48 +0100

    initial commit with all mandatory tasks done

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,8 @@
+export default function createReportObject (employeesList) {
+  return {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments (dept) {
+      return Object.keys(dept).length;
+    }
+  };
+}
