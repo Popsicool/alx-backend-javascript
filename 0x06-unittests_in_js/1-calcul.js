2a3b8d5cfe2878e13b8152e29c30f0069a451aff
Popsicool <akinolasamson1234@gmail.com>
2023-06-14 20:27:42 +0100

    tas 2

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,16 +1,19 @@
 const calculateNumber = (type, a, b) => {
   if (type === 'SUM') {
     return Math.round(a) + Math.round(b);
-  } if (type == 'SUBTRACT') {
+  }
+  if (type == 'SUBTRACT') {
     return Math.round(a) - Math.round(b);
-  } if (type == 'DIVIDE') {
+  }
+  if (type == 'DIVIDE') {
     const first = Math.round(a);
     const second = Math.round(b);
     if (second === 0) {
       return 'Error';
     }
-    return a / b;
+    return first / second;
   }
+  return 0;
 };
 
 module.exports = calculateNumber;
