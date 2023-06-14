51014551e0d4e8508b7dd23a59f5410bd29fe79c
Popsicool <akinolasamson1234@gmail.com>
2023-06-14 19:44:13 +0100

    task 0

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,6 +1,6 @@
 
 const calculateNumber = (a, b) => {
-  return Math.round(a + b);
+  return Math.round(a) + Math.round(b);
 };
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -11,8 +11,8 @@ describe('Test sum by adding numbers', function () {
   it('adding 1.2 and 3.7 to give 5', function () {
     assert.equal(calculateNumber(1.2, 3.7), 5);
   });
-  it('adding 1.5 and 3.7 to give 5', function () {
-    assert.equal(calculateNumber(1.5, 3.7), 5);
+  it('adding 1.5 and 3.7 to give 6', function () {
+    assert.equal(calculateNumber(1.5, 3.7), 6)
   });
   it('adding 0 and 0 to give 0', function () {
     assert.equal(calculateNumber(0, 0), 0);
