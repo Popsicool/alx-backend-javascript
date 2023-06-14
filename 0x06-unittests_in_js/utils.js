42cdbb20a8b6c4607cc7f89f3bfd5ab902c27394
Popsicool <akinolasamson1234@gmail.com>
2023-06-14 20:52:12 +0100

    task 3

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,21 @@
+const Utils = {
+  calculateNumber (type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type == 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type == 'DIVIDE') {
+      const first = Math.round(a);
+      const second = Math.round(b);
+      if (second === 0) {
+        return 'Error';
+      }
+      return first / second;
+    }
+    return 0;
+  }
+};
+
+module.exports = Utils;
