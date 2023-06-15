7a51d7009ef1fd09224715c36f166f543fae1287
Popsicool <akinolasamson1234@gmail.com>
2023-06-15 08:08:34 +0100

    task 9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,18 +1,19 @@
-const express = require("express")
-const PORT = 7865
-const app = express()
+const express = require('express');
+
+const PORT = 7865;
+const app = express();
 
 app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id(\\d+)', (req, res) => {
-  const id = req.params.id
+  const { id } = req.params;
   res.send(`Payment methods for cart ${id}`);
 });
 
 app.listen(PORT, () => {
-    console.log(`API available on localhost port ${PORT}`)
-})
+  console.log(`API available on localhost port ${PORT}`);
+});
 
 module.exports = app;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -4,33 +4,33 @@ const { expect } = require('chai');
 describe('test api integration', () => {
   const url = 'http://localhost:7865';
 
-  it('make a call to the / endpoint', (done) => {
+  it('make a call to the / endpoint', () => new Promise((done) => {
     request.get(`${url}/`, (_err, res, body) => {
-        expect(body).to.be.equal('Welcome to the payment system');
-        expect(res.statusCode).to.be.equal(200);
-        done();
+      expect(body).to.be.equal('Welcome to the payment system');
+      expect(res.statusCode).to.be.equal(200);
+      done();
     });
-  });
+  }));
 
-  it('test GET /cart/:id with valid :id', (done) => {
+  it('test GET /cart/:id with valid :id', () => new Promise((done) => {
     request.get(`${API_URL}/cart/95`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
       expect(body).to.be.equal('Payment methods for cart 95');
       done();
     });
-  });
+  }));
 
-  it('test GET /cart/:id returns 404 with negative :id', (done) => {
+  it('test GET /cart/:id returns 404 with negative :id', () => new Promise((done) => {
     request.get(`${API_URL}/cart/-95`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
-  });
+  }));
 
-  it('test GET /cart/:id returns 404 with uuid :id', (done) => {
+  it('test GET /cart/:id returns 404 with uuid :id', () => new Promise((done) => {
     request.get(`${API_URL}/cart/ad34-sget-5643`, (_err, res, _body) => {
       expect(res.statusCode).to.be.equal(404);
       done();
     });
-  });
+  }));
 });
